abc0f5a149b9bd8faf589b85039efc2809bd8a1e
atomoo <yangpein@gmail.com>
2019-07-26 11:09:00 +0800

    add: destroy record

--- app/controller/records.ts
+++ app/controller/records.ts
@@ -37,4 +37,47 @@ export default class RecordController extends Controller {
         ctx.body = result;
     }
 
+    public async destroy() {
+        const { ctx } = this;
+        const id = ctx.query.id;
+        if (!id) {
+            ctx.status = 400;
+            ctx.body = {
+                ret: '400',
+                msg: 'Invalid Parameter',
+            };
+            return;
+        }
+        const recordFound = await this.ctx.model.Record.findOne<Record>({ where: { id } });
+        if (!recordFound) {
+            ctx.status = 404;
+            ctx.body = {
+                ret: '404',
+                msg: 'Not found',
+            };
+            return;
+        }
+        const updateResult = await ctx.model.Record.update(
+            { deleted: true },
+            {
+                where: { id: recordFound.id! },
+            },
+        );
+        if (updateResult && updateResult[0] === 1) {
+            ctx.status = 200;
+            ctx.body = {
+                ret: '200',
+                msg: 'success',
+            };
+            return;
+        } else {
+            ctx.status = 500;
+            ctx.body = {
+                ret: '500',
+                msg: 'system error',
+            };
+            return;
+        }
+    }
+
 }
